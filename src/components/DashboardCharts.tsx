497b8cefb9121604f4c574bef10feb8de457a496
Ar-Study1 <farrel.hunter85@gmail.com>
2025-08-06 10:36:55 +0700

    Checkpoint

--- src/components/DashboardCharts.tsx
+++ src/components/DashboardCharts.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import RevenueChart from "./RevenueCharts";
-import VoltageChart from "./VoltageCharts";
-import KwhChart from "./KwhCharts";
+// import RevenueChart from "./RevenueCharts";
+// import VoltageChart from "./VoltageCharts";
+// import KwhChart from "./KwhCharts";
 
 const DashboardCharts: React.FC = () => {
   return (
@@ -11,8 +11,8 @@ const DashboardCharts: React.FC = () => {
         className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6 animate-fade-in"
         style={{ animationDelay: "0.3s" }}
       >
-        <VoltageChart data={} />
-        <RevenueChart data={} />
+        {/* <VoltageChart data={} />
+        <RevenueChart data={} /> */}
       </div>
 
       {/* Charts - 1 in second row */}
@@ -20,7 +20,7 @@ const DashboardCharts: React.FC = () => {
         className="grid grid-cols-1 gap-6 mb-6 animate-fade-in"
         style={{ animationDelay: "0.4s" }}
       >
-        <KwhChart data={} />
+        {/* <KwhChart data={} /> */}
       </div>
     </>
   );
